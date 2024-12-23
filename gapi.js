3e1431508a1dd196b64a429b4e36b96097f4ae35
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2024-12-23 21:26:46 +0100

    Add Google API js file containing requests

---
+++ gapi.js
@@ -0,0 +1,27 @@
+async function getEvents() {
+    const ACCESS_TOKEN = localStorage.getItem("bearer_token");
+    const CALENDAR_ID = "primary"; // Or a specific calendar ID
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${ACCESS_TOKEN}`,
+          },
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const events = await response.json();
+      console.log("Events:", events);
+    } catch (error) {
+      console.error("Error fetching events:", error);
+    }
+  }
+  
+
+export {getEvents}
