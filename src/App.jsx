68dd8994eb5b285b15cf3b6cdd2d3258b2b1f972
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2025-01-08 09:02:45 +0100

    Change backend request URL's

--- src/App.jsx
+++ src/App.jsx
@@ -106,6 +106,7 @@ const endMeetingTrigger = async () => {
       const data = await fetch("https://room-display-backend-johannesfyren-johannesfyrens-projects.vercel.app/refreshAccessToken", { //"http://localhost:3000/refreshAccessToken"
         method: "POST",
         headers: { "Content-Type": "application/json" },
+        mode: "no-cors",
         body: JSON.stringify({
           refresh_token: localStorage.getItem("refresh_token"),
         }),
